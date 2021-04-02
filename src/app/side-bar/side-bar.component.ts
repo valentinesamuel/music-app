b3ee93a139a8b5f1587da3cd9af14e96ef51061c
valentinesamuel <valentinesamuel2580@gmail.com>
2021-04-02 14:37:39 +0100

    (FEAT): Styles the side menu section

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../services/spotify.service';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBookReader } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-bar',
@@ -9,8 +12,14 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-  faCoffee = faCoffee;
   faHome = faHome;
+  faBookReader = faBookReader;
+  faSearch = faSearch;
+  faHeart = faHeart;
+  faPlusSquare = faPlusSquare;
+  searchActive = true;
+  homeActive = true;
+  libraryActive = true;
   constructor(private spotifyServ: SpotifyService) { }
 
   ngOnInit(): void {
@@ -19,5 +28,28 @@ export class SideBarComponent implements OnInit {
 
     // });
   }
+  goHome(){
+    this.homeActive = !this.homeActive;
+    console.log(this.homeActive);
+    
+    setTimeout(() => {
+      this.homeActive = !this.homeActive;
+    }, 1000);
+  }
 
+  goToSearch() {
+    this.searchActive = !this.searchActive;
+    console.log(this.searchActive);
+    setTimeout(() => {
+      this.searchActive = !this.searchActive;
+    }, 1000);
+  }
+
+  goToLibrary() {
+    this.libraryActive = !this.libraryActive;
+    console.log(this.libraryActive);
+    setTimeout(() => {
+      this.libraryActive = !this.libraryActive;
+    }, 1000);
+  }
 }
