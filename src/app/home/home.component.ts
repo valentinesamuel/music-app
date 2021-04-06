3be8e1ae9e4c04fe86b7369fa5835c6b61de3024
valentinesamuel <valentinesamuel2580@gmail.com>
2021-04-06 00:11:18 -0700

    (FEAT): This project is going to be lit...
    (PLUS): This ui almost looks like that of spotify

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,15 +17,15 @@ export class HomeComponent implements OnInit {
   constructor(private spotifyServ: SpotifyService, private router: Router) { }
 
   ngOnInit(): void {
-    // this.spotifyServ.browseCategories().subscribe((categories) => {
-    //   this.categoriesData = categories;
-    //   this.categoriesData = this.categoriesData.categories.items
-    // });
+    this.spotifyServ.browseCategories().subscribe((categories) => {
+      this.categoriesData = categories;
+      this.categoriesData = this.categoriesData.categories.items
+    });
 
-    // this.spotifyServ.getTopArtists().subscribe((topArtists) => {
-    //   this.topArtistData = topArtists;
-    //   this.topArtistData= this.topArtistData.items
-    // })
+    this.spotifyServ.getTopArtists().subscribe((topArtists) => {
+      this.topArtistData = topArtists;
+      this.topArtistData= this.topArtistData.items
+    })
   }
   
 }
