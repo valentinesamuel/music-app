2e2ff2c2463853fd7b6d82f1051518bd761ea54c
valentinesamuel <valentinesamuel2580@gmail.com>
2021-04-19 20:43:06 -0700

    (FEAT): Changing th epause and play button on click

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart,faPlayCircle, faRandom, faRedo, faForward, faBackward, faPauseCircle, faPause } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,10 +7,27 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  status = true
+  stateIcon = faPlayCircle;
   faHeart = faHeart;
+  faPlayCircle = faPlayCircle;
+  faRandom = faRandom;
+  faRedo = faRedo;
+  faForward = faForward;
+  faPauseCircle = faPauseCircle;
+  faBackward = faBackward;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onPauseAndPlay() {
+    this.status = !this.status;
+    if (this.status) {
+        this.stateIcon = faPlayCircle;
+    } else {
+      this.stateIcon = faPauseCircle;
+    }
+    
+  }
 }
